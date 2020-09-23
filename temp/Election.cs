98ff0ad243bea6c3d8006af61aad09fbcdbebde9
rodrigocamargo <rodrigocamargo854@gmail.com>
2020-09-23 16:01:26 -0300

    refatorando...

--- temp/Election.cs
+++ temp/Election.cs
@@ -5,29 +5,86 @@ using System.Linq;
 
 namespace temp
 {
-    public class Election
+  public class Election
     {
-        //Creating the  first prop
-
-        public List<(int id, string name)> Candidate { get; set; }
-
-        public bool CreatedCandidates(List<(int id, string name)> candidate, string pass)
+        // Propriedade abaixo:
+        // Sempre em PascalCase
+        public List<(Guid id, string name, string cpf, int votes)> Candidates { get; set; }
+        
+        public bool CreateCandidates(List<(string names, string cpf)> candidateNames, string password)
         {
-
-            if ( pass == "CaDaStRaR")
+            if (password == "Pa$$w0rd")
             {
-                Candidate = candidate.Select(item => {
-                    return(item.id, item.name);
+                Candidates = candidateNames.Select(candidateName => {
+                    return (Guid.NewGuid(), candidateName.names,candidateName.cpf, 0);
                 }).ToList();
+
                 return true;
-            
             }
-            return false;
-        }   
+
+            else
+            {
+                return false;
+            }
+        }
+
        
 
+        // ToDo: Criar método que retorne um Guid que represente o candidato pesquisado por CPF
+        // public Guid ReturnCandidateByCpf(string cpf)
 
-    }
+        // {
+            
+        // }
+
+
+        // ToDo: Este método deve retornar a lista de candidatos que tem o mesmo nome informado
+        // public List<string>  ReturnRepeatNames (List<(string name, string cpf)> candidates)
+        // {
+        //     List<string> repeatNames = new List<string>();
+
+        //     for (int i = 0; i < candidates.Count; i++)
+        //     {
+        //         repeatNames = candidates.Where(item => item.name == candidates[i].name);
+        //     }
+
+        //     return repeatNames;
+
+        // }
+
+       public Guid GetCandidateIdByName(string name, string cpf)
+        {
+            return Candidates.First(x => x.name == name).id;
+        }
+        
+        // public void Vote(Guid id)
+        // {
+        //     Candidates = Candidates.Select(candidate => {
+        //         return candidate.id == id ? (Guid.NewGuid(), 
+        //         candidate.name, candidate.cpf, candidate.votes + 1)
+        //         : candidate;
+        //     }).ToList();
+        // }
+
+    //     public List<(Guid id, string name,string cpf, int votes)> GetWinners()
+    //     {
+    //         var winners = new List<(Guid id, string name,string cpf, int votes)>{Candidates[0]};
+
+    //         for (int i = 1; i < Candidates.Count; i++)
+    //         {
+    //             if (Candidates[i].votes > winners[0].votes)
+    //             {
+    //                 winners.Clear();
+    //                 winners.Add(Candidates[i]);
+    //             }
+    //             else if (Candidates[i].votes == winners[0].votes)
+    //             {
+    //                 winners.Add(Candidates[i]);
+    //             }
+    //         }
+    //         return winners;
+    //     }
+     }
 
 
 }
--- temp/ElectionTest.cs
+++ temp/ElectionTest.cs
@@ -4,41 +4,141 @@ using System.Collections.Generic;
 
 namespace temp
 {
-    public class ElectionTest// criando o teste primeiro
+    public class ElectionTest
     {
         [Fact]
-        //não deve criar candidatos se a senha estiver errada
-        public void Shold_Be_Not_created_Candidates_if_the_Pass_is_Incorrect()
+        public void should_not_create_candidates_when_password_is_incorrect()
         {
-            //Given
-            var created = new Election();
-            var candidates = new List<(int id, string name)> { (1, "jose") };
-
-            //Then
-            var result = created.CreatedCandidates(candidates, "rewrwrwr");
-
-            //When
-
-            Assert.False(result);
-        }
-        [Fact]
-        public void Shold_Be__created_Candidates_if_the_Pass_is_Correct()
-        {
-            //Given
+            // Dado / Setup
             var election = new Election();
-            (int id, string name) candidate = (1, "jose");
-            var candidates = new List<(int id, string name)> { candidate };
+            var candidates = new List<(string name, string cpf)> { ("José", "8929831321") };
 
-            //When
-            var result = election.CreatedCandidates(candidates, "CaDaStRaR"); // pass certo
+            // Quando / Ação
+            var created = election.CreateCandidates(candidates, "incorrect");
 
-            //Then
-            Assert.True(election.Candidate.Count == 1);
-            Assert.True(candidate.id == election.Candidate[0].id);
-            Assert.True(candidate.name == election.Candidate[0].name);
+            // Deve / Asserções
+            Assert.Null(election.Candidates);
+            Assert.False(created);
+        }
 
+        [Fact]
+        public void should_create_candidates_when_password_is_correct()
+        {
+            // Dado / Setup
+
+            // OBJETO election
+            var election = new Election();
+            (string candidateName, string cpf) candidate = ("José", "9078790");
+            var candidates = new List<(string name, string cpf)> { (candidate) };
+
+            // Quando / Ação
+
+            // Estamos acessando o MÉTODO CreateCandidates do OBJETO election
+            var created = election.CreateCandidates(candidates, "Pa$$w0rd");
+
+            // Deve / Asserções
+            Assert.True(created);
+
+            // Estamos acessando a PROPRIEDADE Candidates, que faz parte do ESTADO do OBJETO election
+            Assert.True(election.Candidates.Count == 1);
+            Assert.True(election.Candidates[0].name == candidate.candidateName);
         }
 
 
+            [Fact]
+        public void should_return_different_ids()
+        {
+            var election = new Election();
+            (string jose,string cpfJose) = ("Jose","342342543");
+            (string adilson,string cpfAdilson) = ("adilson","342342543");
+
+            var candidates = new List<(string name, string cpf)> {(jose, adilson)};
+            election.CreateCandidates(candidates, "Pa$$w0rd");
+
+            var joseId = election.GetCandidateIdByName(jose,cpfJose);
+            var adilsonId = election.GetCandidateIdByName(adilson,cpfAdilson);
+
+            Assert.True(joseId != adilsonId);
+        }
+        //     [Fact]
+        //     public void should_vote_twice_in_candidate_Fernando()
+        //     {
+        //         // Dado / Setup
+        //         // OBJETO election
+        //         var election = new Election();
+        //         (string name, string cpf) fernando = ("fernando", "34252523");
+        //         (string name, string cpf) adilson = ("adilson", "4242343");
+        //         var candidate1 = new List<(string name, string cpf)> { (fernando) };
+        //         var candidate2 = new List<(string name, string cpf)> { (adilson) };
+
+
+        //         election.CreateCandidates(candidate1, "Pa$$w0rd");
+        //         var fernandoId = election.GetCandidateIdByName(fernando);
+        //         var adilsonId = election.GetCandidateIdByName(adilson);
+
+        //         // Quando / Ação
+        //         // Estamos acessando o MÉTODO ShowMenu do OBJETO election
+        //         election.Vote(fernandoId);
+        //         election.Vote(fernandoId);
+
+        //         // Deve / Asserções
+        //         var candidateFernando = election.Candidates.Find(x => x.id == fernandoId);
+        //         var candidateAdilson = election.Candidates.Find(x => x.id == adilsonId);
+        //         Assert.Equal(2, candidateFernando.votes);
+        //         Assert.Equal(0, candidateAna.votes);
+        //     }
+
+        //     [Fact]
+        //     public void should_return_Ana_as_winner_when_only_Ana_receives_votes()
+        //     {
+        //         // Dado / Setup
+        //         // OBJETO election
+        //         var election = new Election();
+        //         (string name, string cpf) CandidateJose = ("José", "9679869867");
+        //         (string name, string cpf) CandidateAna = ("Ana", "43534554");
+        //         var candidates = new List<(string name, string cpf)> { (CandidateJose), (CandidateAna) };
+        //         election.CreateCandidates(candidates, "Pa$$w0rd");
+
+        //         var anaId = election.GetCandidateIdByName(ana);
+
+        //         // Quando / Ação
+        //         // Estamos acessando o MÉTODO ShowMenu do OBJETO election
+        //         election.Vote(anaId);
+        //         election.Vote(anaId);
+        //         var winners = election.GetWinners();
+
+        //         // Deve / Asserções
+        //         Assert.True(winners.Count == 1);
+        //         Assert.Equal(anaId, winners[0].id);
+        //         Assert.Equal(2, winners[0].votes);
+        //     }
+
+        //     [Fact]
+        //     public void should_return_both_candidates_when_occurs_draw()
+        //     {
+        //         // Dado / Setup
+        //         // OBJETO election
+        //         var election = new Election();
+        //         string fernando = "Fernando";
+        //         string ana = "Ana";
+        //         var candidates = new List<string> { fernando, ana };
+        //         election.CreateCandidates(candidates, "Pa$$w0rd");
+        //         var fernandoId = election.GetCandidateIdByName(fernando);
+        //         var anaId = election.GetCandidateIdByName(ana);
+
+        //         // Quando / Ação
+        //         // Estamos acessando o MÉTODO ShowMenu do OBJETO election
+        //         election.Vote(anaId);
+        //         election.Vote(fernandoId);
+        //         var winners = election.GetWinners();
+
+        //         // Deve / Asserções
+        //         var candidateFernando = winners.Find(x => x.id == fernandoId);
+        //         var candidateAna = winners.Find(x => x.id == anaId);
+        //         Assert.Equal(1, candidateFernando.votes);
+        //         Assert.Equal(1, candidateAna.votes);
+        //     }
+        // }
     }
 }
+
